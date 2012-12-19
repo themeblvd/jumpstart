05a97aec784291e5f6ca426104a4a59f2e8bd147
Jason Bobich <jasonbobich@gmail.com>
2012-12-18 15:21:46 -0900

    Adjustments to searchform to be 100% width.

--- searchform.php
+++ searchform.php
@@ -2,9 +2,8 @@
 <div class="themeblvd-search">
     <form method="get" action="<?php echo home_url( '/' ); ?>">
 		<div class="controls">
-			<div class="input-append">
-				<input type="text" class="search-input" name="s" placeholder="<?php echo $text; ?>" /><button class="btn btn-default" type="submit"><i class="icon-search"></i></button>
-			</div>
+			<input type="text" class="search-input" name="s" placeholder="<?php echo $text; ?>" />
+			<button class="btn btn-default" type="submit"><i class="icon-search"></i></button>
 		</div>
     </form>
 </div>
