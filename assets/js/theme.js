e6b1c3a8568887e2cf809e0a04b4d56484669c44
Jason Bobich <jasonbobich@gmail.com>
2013-01-07 14:20:56 -0900

    Changed responsive viewport sizes to match Bootstrap.
    Tablets: 979px - 767px
    
    Mobile:  767px and below

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -3,6 +3,6 @@ jQuery(document).ready(function($){
 	/* Put any theme-specific JS here... */
 	
 	// Enable toggle for Main menu on tablets and mobile
-	$('.btn-navbar').toggleMenu({'viewport':768});
+	$('.btn-navbar').toggleMenu({'viewport':979});
 
 });
