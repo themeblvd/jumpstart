6a01e700c952333ee7a690775c8657047bd7b90d
Jason Bobich <jasonbobich@gmail.com>
2013-07-02 17:57:22 -0500

    Removed wp_feed_cache_transient_lifetime filter because we no longer use RSS for Twitter.

--- framework/themeblvd.php
+++ framework/themeblvd.php
@@ -102,7 +102,6 @@ if ( is_admin() ) {
 	add_filter( 'oembed_result', 'themeblvd_oembed_result', 10, 2 );
 	add_filter( 'embed_oembed_html', 'themeblvd_oembed_result', 10, 2 );
 	add_filter( 'wp_audio_shortcode', 'themeblvd_audio_shortcode' );
-	add_filter( 'wp_feed_cache_transient_lifetime' , 'themeblvd_feed_transient' );
 	add_filter( 'themeblvd_the_content', 'wptexturize' );
 	add_filter( 'themeblvd_the_content', 'wpautop' );
 	add_filter( 'themeblvd_the_content', 'shortcode_unautop' );
