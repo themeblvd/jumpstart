239bb1437dab8629e0c37540d7452928d9be83c4
Jason Bobich <jasonbobich@gmail.com>
2013-07-06 10:54:42 -0800

    Some JS foramtting cleanup.

--- framework/assets/js/themeblvd.js
+++ framework/assets/js/themeblvd.js
@@ -4,44 +4,44 @@
 
 (function($) {
     $.fn.toggleMenu = function(options) {
-        
+
         var settings = $.extend({
-	      'viewport' 	: 768,
-	      'openClass'	: 'mobile-open',
-	      'closedClass'	: 'mobile-closed'
-	    }, options);
-        
+			'viewport'		: 768,
+			'openClass'		: 'mobile-open',
+			'closedClass'	: 'mobile-closed'
+		}, options);
+
         // Run it
         this.each(function(){
 
-        	var el = $(this),
-        		target = $(el.attr('href')),
-        		currentViewport,
-        		timeout = false;
-		    
-		    // Toggle on click
-		    el.click(function(){
-			    if( target.hasClass(settings.openClass) )
-				    target.slideUp().removeClass(settings.openClass).addClass(settings.closedClass);
-			    else
-			    	target.slideDown().removeClass(settings.closedClass).addClass(settings.openClass);
+			var el = $(this),
+				target = $(el.attr('href')),
+				currentViewport,
+				timeout = false;
 
-			    return false;
-		    });
-		    
-		    // Window re-sizing  - For those people screwing with the 
-		    // browser window and are not actually on a mobile device.
+			// Toggle on click
+			el.click(function(){
+				if( target.hasClass(settings.openClass) )
+					target.slideUp().removeClass(settings.openClass).addClass(settings.closedClass);
+				else
+					target.slideDown().removeClass(settings.closedClass).addClass(settings.openClass);
+
+				return false;
+			});
+
+			// Window re-sizing  - For those people screwing with the
+			// browser window and are not actually on a mobile device.
 		    $( window ).resize( function() {
 				if ( false !== timeout )
 					clearTimeout( timeout );
-		
+
 				timeout = setTimeout( function() {
 					currentViewport = $(window).width();
 					if( currentViewport > settings.viewport )
 					{
-						// Add class "expanded" so we can keep track of 
-						// whether this re-sizing is occuring on a mobile 
-						// device or not. If we're on mobile, the "forced_open" 
+						// Add class "expanded" so we can keep track of
+						// whether this re-sizing is occuring on a mobile
+						// device or not. If we're on mobile, the "forced_open"
 						// class should never get added.
 						target.show().removeClass(settings.openClass+' '+settings.closedClass).addClass('expanded');
 					}
@@ -51,7 +51,7 @@
 						if( target.hasClass('expanded') )
 							target.hide().removeClass('expanded');
 					}
-						
+
 				}, 100 );
 			});
         });
@@ -67,83 +67,86 @@ jQuery(document).ready(function($) {
 	// ---------------------------------------------------------
 	// Menus
 	// ---------------------------------------------------------
-	
+
 	if(themeblvd.superfish)
 	{
 		// Activate Superfish
 		$('ul.sf-menu').superfish().addClass('sf-menu-with-fontawesome');
-		
+
 		// Adjust sub indicators to use fontawesome
 		$('ul.sf-menu-with-fontawesome > li > a .sf-sub-indicator').replaceWith('<i class="sf-sub-indicator icon-caret-down"></i>');
 		$('ul.sf-menu-with-fontawesome ul li a .sf-sub-indicator').replaceWith('<i class="sf-sub-indicator icon-caret-right"></i>');
 	}
-	
+
 	// Allow bootstrap "nav-header" class in menu items.
 	// Note: For primary navigation will only work on levels 2-3
 	// (1) ".sf-menu li li.nav-header" 	=> Primary nav dropdowns
 	// (2) ".menu li.nav-header" 		=> Standard custom menu widget
 	// (3) ".subnav li.nav-header" 		=> Theme Blvd Horizontal Menu widget
 	var menu_text;
-	$('.sf-menu li li.nav-header, .menu li.nav-header, .subnav li.nav-header').each(function(){	
+	$('.sf-menu li li.nav-header, .menu li.nav-header, .subnav li.nav-header').each(function(){
 		menu_text = $(this).find('> a').text();
 		$(this).prepend('<span>'+menu_text+'</span>').find('> a').remove();
 	});
-	
+
 	// Allow bootstrap "divider" class in menu items.
 	// Note: For primary navigation will only work on levels 2-3
 	$('.sf-menu li li.divider, .menu li.divider').html('');
-	
+
 	// Fontawesome icons in menu items
 	var classes, name;
 	$('[class^="menu-icon-"]').each(function(){
-		// For this to work, "menu-icon-whatever" must be the first 
+		// For this to work, "menu-icon-whatever" must be the first
 		// class inputted on the menu item.
 		classes = $(this).attr('class');
 		name = classes.substr(classes.indexOf('menu-icon') + 10).split(' ')[0];
 		$(this).find('> a, > span').prepend('<i class="icon-'+name+'"></i>');
 	});
-		
+
 	// ---------------------------------------------------------
 	// No-click dropdowns
 	// ---------------------------------------------------------
-	
+
 	$('ul.sf-menu .no-click').find('a:first').click(function(){
 		return false;
 	});
-	
+
 	// ---------------------------------------------------------
 	// Gallery Shortcode Integration
 	// ---------------------------------------------------------
-	
-	// Since our gallery integration is specifically designed 
-	// to work with prettyPhoto, if prettyPhoto script isn't 
-	// included, we'll halt it all together. 
+
+	// Since our gallery integration is specifically designed
+	// to work with prettyPhoto, if prettyPhoto script isn't
+	// included, we'll halt it all together.
 	if(themeblvd.prettyphoto)
 	{
 		$('.gallery').append('<div class="clear"></div>');
-	
+
 		$('.gallery').each(function(){
-	
+
 			var current_gallery = $(this),
 				gallery_id = current_gallery.attr('id');
-			
+
 			current_gallery.find('.gallery-item a').each(function(){
+
 				// Add bootstrap thumbnail class
 				$(this).find('img').addClass('thumbnail');
+
 				// Append lightbox if it's an image
-				if(this.href.match(/\.(jpe?g|png|bmp|gif|tiff?)$/i)){
+				if(this.href.match(/\.(jpe?g|png|bmp|gif|tiff?)$/i)) {
 				    $(this).attr('rel','themeblvd_lightbox['+gallery_id+']');
 				    $(this).addClass('image-button');
 				}
+
 			});
-		
+
 		});
 	}
-	
+
 	// ---------------------------------------------------------
 	// Lightbox
 	// ---------------------------------------------------------
-	
+
 	// Check for prettyPhoto script.
 	if(themeblvd.prettyphoto)
 	{
@@ -155,12 +158,12 @@ jQuery(document).ready(function($) {
 			show_title: false
 		});
 	}
-	
+
 	// Animations on lightbox thumbnails.
 	if(themeblvd.thumb_animations)
 	{
 		$('a[rel^="themeblvd_lightbox"]').prepend('<span class="enlarge"></span>');
-		
+
 		$('a[rel^="themeblvd_lightbox"]').hover(
 			function () {
 				var el = $(this);
@@ -170,7 +173,7 @@ jQuery(document).ready(function($) {
 				el.find('img').stop(true, true).animate({
 					opacity: 0.6
 				}, 100 );
-			}, 
+			},
 			function () {
 				var el = $(this);
 				el.find('.enlarge').stop(true, true).animate({
@@ -182,11 +185,11 @@ jQuery(document).ready(function($) {
 			}
 		);
 	}
-	
+
 	// ---------------------------------------------------------
 	// Featured Image overlay links
 	// ---------------------------------------------------------
-	
+
 	if(themeblvd.featured_animations || themeblvd.image_slide_animations)
 	{
 		var selector = "";
@@ -206,7 +209,7 @@ jQuery(document).ready(function($) {
 				el.find('.image-overlay-icon').stop(true, true).animate({
 					opacity: 1
 				}, 300 );
-			}, 
+			},
 			function () {
 				var el = $(this);
 				el.find('.image-overlay-bg').stop(true, true).animate({
@@ -218,25 +221,25 @@ jQuery(document).ready(function($) {
 			}
 		);
 	}
-	
+
 	// ---------------------------------------------------------
 	// Jump Menu
 	// ---------------------------------------------------------
-	
-	$(".tb-jump-menu").change(function(e) {
+
+	$(".tb-jump-menu").change(function() {
 		window.location.href = $(this).val();
-	})
-	
+	});
+
 	// ---------------------------------------------------------
 	// Logo w/retina display support
 	// ---------------------------------------------------------
-	
+
 	if(themeblvd.retina_logo)
 	{
 		var image = $('.tb-image-logo img'),
 			image_2x = image.attr('data-image-2x');
-		
-		// If a retina-otimized image was detected 
+
+		// If a retina-otimized image was detected
 		// and should be displayed
 		if(window.devicePixelRatio >= 1.5 && image_2x)
 		{
@@ -246,7 +249,7 @@ jQuery(document).ready(function($) {
 			});
 		}
 	}
-	
+
 	// ---------------------------------------------------------
 	// Bootstrap Integration
 	// ---------------------------------------------------------
@@ -256,9 +259,9 @@ jQuery(document).ready(function($) {
 
 		// Add standard table classes to calendar widget
 		$('#calendar_wrap table').addClass('table table-bordered');
-		
+
 		// Tabs - Automatic fixed height
-		// This allows the user to have the set of tabs 
+		// This allows the user to have the set of tabs
 		// automatically stay the height of the tallest tab.
 		$('.tabbable.fixed-height').each(function(){
 			var tallest = 0;
@@ -271,19 +274,19 @@ jQuery(document).ready(function($) {
 		});
 
 		// Collapsables expanded
-		// This basically just toggles the Plus/Minus fontawesome 
+		// This basically just toggles the Plus/Minus fontawesome
 		// icon we've incorporated into the triggers for the toggles.
 		$('.collapse').on('show', function() {
-			// Toggle is opening, add "active-trigger" class and 
+			// Toggle is opening, add "active-trigger" class and
 			// change icon to a minus sign.
 			$(this).closest('.accordion-group').find('.accordion-toggle').addClass('active-trigger').find('.switch-me').removeClass('icon-plus-sign').addClass('icon-minus-sign');
 		});
 		$('.collapse').on('hide', function() {
-			// Toggle is closing, remove "active-trigger" class and 
+			// Toggle is closing, remove "active-trigger" class and
 			// change icon to a plus sign.
 			$(this).closest('.accordion-group').find('.accordion-toggle').removeClass('active-trigger').find('.switch-me').removeClass('icon-minus-sign').addClass('icon-plus-sign');
 		});
-		// And now if the user has wrapped a set of "toggles" into an 
+		// And now if the user has wrapped a set of "toggles" into an
 		// accordian, this will attach them all.
 		var accordion_id;
 		$('.tb-accordion').each(function(){
@@ -293,5 +296,5 @@ jQuery(document).ready(function($) {
 			});
 		});
 	}
-	
+
 });
