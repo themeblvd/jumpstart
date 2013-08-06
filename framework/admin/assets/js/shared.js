0c6318387b7ae877b7021ff635299b07a965b334
Jason Bobich <jasonbobich@gmail.com>
2013-08-05 17:09:33 -0800

    Fixed Firefox media uploder bug.

--- framework/admin/assets/js/shared.js
+++ framework/admin/assets/js/shared.js
@@ -1,5 +1,5 @@
 /**
- * Prints out the inline javascript that is shared between all 
+ * Prints out the inline javascript that is shared between all
  * framework admin areas.
  */
 
@@ -7,12 +7,12 @@
 
 	// Setup methods for themeblvd namespace
 	var themeblvd_shared = {
-    	
+
     	// All general binded events
     	init : function()
     	{
     		var $this = this;
-    	
+
     		// Toggle widgets
 			$this.find('.widget-name-arrow').live('click', function(){
 				var el = $(this), closed = el.closest('.widget-name').hasClass('widget-name-closed');
@@ -28,14 +28,14 @@
 				}
 				return false;
 			});
-			
+
 			// Help tooltips
 			$this.find('.tooltip-link').live('click', function(){
 				var message = $(this).attr('title');
 				tbc_confirm(message, {'textOk':'Ok'});
-				return false;						
+				return false;
 			});
-			
+
 			// Delete item by ID passed through link's href
 			$this.find('.delete-me').live('click', function(){
 				var item = $(this).attr('href');
@@ -47,26 +47,26 @@
 			        }
 			    });
 			    return false;
-			});	
-			
+			});
+
 			// Fancy Select
 			$this.find('.tb-fancy-select').each(function(){
-				var el = $(this), 
+				var el = $(this),
 					value = el.find('select').val(),
 					text = el.find('option[value="'+value+'"]').text();
 				el.find('.textbox').text(text);
 			});
     	},
-    	
+
     	// Setup custom options
     	options : function( type )
     	{
     		return this.each(function(){
-				
-				var $this = $(this);	
-			
-	    		// Apply all actions that need applying when an 
-	    		// option set is loaded. This will be called any 
+
+				var $this = $(this);
+
+	    		// Apply all actions that need applying when an
+	    		// option set is loaded. This will be called any
 	    		// time a new options set is inserted.
 	    		if(type == 'setup')
 	    		{
@@ -76,11 +76,11 @@
 						var el = $(this), value = el.find('select').val(), text = el.find('option[value="'+value+'"]').text();
 						el.find('.textbox').text(text);
 					});
-			
+
 	    			// Custom content
 	    			$this.find('.custom-content-types').each(function(){
 	    				var el = $(this), value = el.find('select').val(), parent = el.closest('.subgroup');
-	    				
+
 	    				if(value == 'external')
 	    					parent.find('.page-content').show();
 	    				else if (value == 'raw')
@@ -89,7 +89,7 @@
 	    					parent.find('.widget_area-content').show();
 
 	    			});
-	    			
+
 	    			// Columns only
 	    			$this.find('.columns').each(function(){
 	    				var el = $(this), i = 1, num = el.find('.column-num').val();
@@ -102,7 +102,7 @@
 							i++;
 	    				}
 	    			});
-	    			
+
 	    			// Tabs only
 	    			$this.find('.tabs').each(function(){
 	    				var el = $(this), i = 1, num = el.find('.tabs-num').val();
@@ -115,35 +115,35 @@
 							i++;
 	    				}
 	    			});
-	    			
+
 	    			// Columns AND Tabs
 	    			$this.find('.section-content').each(function(){
     					var section = $(this), type = section.find('.column-content-types select.select-type').val();
     					section.find('.column-content-type').hide();
     					section.find('.column-content-type-'+type).show();
     				});
-    				
+
     				// Show/Hide groupings
     				$this.find('.show-hide').each(function(){
     					var el = $(this), checkbox = el.find('.trigger input');
     					if( checkbox.is(':checked') )
     						el.find('.receiver').show();
     				});
-    				
+
     				// Show/Hide toggle grouping (triggered with <select> to target specific options)
     				$this.find('.show-hide-toggle').each(function(){
     					var el = $(this), value = el.find('.trigger select').val();
     					el.find('.receiver').hide();
     					el.find('.receiver-'+value).show();
     				});
-    				
+
     				// Configure logo
     				$this.find('.section-logo').each(function(){
     					var el = $(this), value = el.find('.select-type select').val();
 						el.find('.logo-item').hide();
-						el.find('.'+value).show();					
+						el.find('.'+value).show();
     				});
-    				
+
     				// Configure social media buttons
     				$this.find('.section-social_media').each(function(){
     					var el = $(this);
@@ -154,9 +154,9 @@
 	    						checkbox.closest('.item').addClass('active').find('.social_media-input').show();
 	    					else
 	    						checkbox.closest('.item').removeClass('active').find('.social_media-input').hide();
-						});					
+						});
     				});
-    				
+
     				// Google font selection
     				$this.find('.section-typography .of-typography-face').each(function(){
     					var el = $(this), value = el.val();
@@ -165,14 +165,14 @@
     					else
     						el.closest('.section-typography').find('.google-font').hide();
     				});
-    				
+
     				// Homepage Content
 	    			$this.find('#section-homepage_content').each(function(){
     					var value = $(this).find('input:checked').val();
     					if( value != 'custom_layout' )
     						$this.find('#section-homepage_custom_layout').hide();
     				});
-    				
+
     				// Color Picker
 					$this.find('.colorSelector').each(function(){
 						var Othis = this; //cache a copy of the this variable for use inside nested function
@@ -193,28 +193,28 @@
 							}
 						});
 					});
-    				
+
     				// Image Options
 					$this.find('.of-radio-img-img').click(function(){
 						$(this).parent().parent().find('.of-radio-img-img').removeClass('of-radio-img-selected');
-						$(this).addClass('of-radio-img-selected');		
-					});	
+						$(this).addClass('of-radio-img-selected');
+					});
 					$this.find('.of-radio-img-label').hide();
 					$this.find('.of-radio-img-img').show();
 					$this.find('.of-radio-img-radio').hide();
-	    				
+
 	    		}
 	    		// Apply all binded actions. This will only need
 	    		// to be called once on the original page load.
 	    		else if(type == 'bind')
 	    		{
-	    			
+
 	    			// Fancy Select
 	    			$this.find('.tb-fancy-select select').live('change', function(){
 		    			var el = $(this), value = el.val(), text = el.find('option[value="'+value+'"]').text();
 						el.closest('.tb-fancy-select').find('.textbox').text(text);
 	    			});
-									
+
 	    			// Custom content
 	    			$this.find('.custom-content-types select').live('change', function(){
 	    				var el = $(this), value = el.val(), parent = el.closest('.subgroup');
@@ -243,7 +243,7 @@
 	    					parent.find('.page-content').hide();
 	    				}
 	    			});
-	    			
+
 	    			// Column widths and number
 	    			$this.find('.columns .column-num').live('change', function(){
 	    				var el = $(this), i = 1, num = el.val(), parent = el.closest('.columns');
@@ -256,7 +256,7 @@
 							i++;
 	    				}
 	    			});
-	    			
+
 	    			// Tabs number
 	    			$this.find('.tabs .tabs-num').live('change', function(){
 	    				var el = $(this), i = 1, num = el.val(), parent = el.closest('.tabs');
@@ -269,23 +269,23 @@
 							i++;
 	    				}
 	    			});
-	    			
+
 	    			// Column/Tab content types
 	    			$this.find('.column-content-types select.select-type').live('change', function(){
 						var section = $(this).closest('.section-content'), type = $(this).val();
 	    				section.find('.column-content-type').hide();
 	    				section.find('.column-content-type-'+type).show();
 	    			});
-	    			
+
 	    			// Show/Hide groupings
     				$this.find('.show-hide .trigger input').live('click', function(){
     					var checkbox = $(this);
     					if( checkbox.is(':checked') )
     						checkbox.closest('.show-hide').find('.receiver').fadeIn('fast');
     					else
-    						checkbox.closest('.show-hide').find('.receiver').hide();			
+    						checkbox.closest('.show-hide').find('.receiver').hide();
     				});
-    				
+
     				// Show/Hide toggle grouping (triggered with <select> to target specific options)
     				$this.find('.show-hide-toggle .trigger select').live('change', function(){
     					var el = $(this), value = el.val(), group = el.closest('.show-hide-toggle');
@@ -297,9 +297,9 @@
     				$this.find('.section-logo .select-type select').live('change', function(){
     					var el = $(this), parent = el.closest('.section-logo'), value = el.val();
 						parent.find('.logo-item').hide();
-						parent.find('.'+value).show();					
+						parent.find('.'+value).show();
     				});
-    				
+
     				// Configure social media buttons
     				$this.find('.section-social_media .checkbox').live('click', function(){
     					var checkbox = $(this);
@@ -308,7 +308,7 @@
     					else
     						checkbox.closest('.item').removeClass('active').find('.social_media-input').hide();
     				});
-    				
+
     				// Google font selection
     				$this.find('.section-typography .of-typography-face').live('change', function(){
     					var el = $(this), value = el.val();
@@ -319,55 +319,55 @@
     				});
 
     				// Homepage Content
-	    			$this.find('#section-homepage_content input:checked').live('change', function() {  					
+	    			$this.find('#section-homepage_content input:checked').live('change', function() {
     					if( $(this).val() == 'custom_layout' )
     						$this.find('#section-homepage_custom_layout').fadeIn('fast');
     					else
-    						$this.find('#section-homepage_custom_layout').fadeOut('fast');		
+    						$this.find('#section-homepage_custom_layout').fadeOut('fast');
     				});
 	    		}
-	    		// Apply media uploader from themeblvd_media_uploader object. 
+	    		// Apply media uploader from themeblvd_media_uploader object.
 	    		// This incorporates the Media Uploader in WP 3.5+
 	    		else if(type == 'media-uploader')
 	    		{
 	    			// Check to make sure wp.media object exists.
-	    			// If it doesn't ... 
-	    			// (1) We're using an older version of WP and the 
+	    			// If it doesn't ...
+	    			// (1) We're using an older version of WP and the
 	    			// legacy uploader.
 	    			// (2) We're using a plugin that uses legacy uploader.
-	    			// (3) the WP 3.5+'s media uploader JS files haven't been 
-	    			// enqueued properly. 
+	    			// (3) the WP 3.5+'s media uploader JS files haven't been
+	    			// enqueued properly.
 	    			if(typeof wp !== 'undefined' && typeof wp.media !== 'undefined')
 	    				themeblvd_media_uploader.init($this);
 	    		}
-    		
+
     		});
     	},
-    	
-    	// Widgets		
+
+    	// Widgets
 		widgets : function()
 		{
 			return this.each(function(){
 				var el = $(this);
 				el.find('.widget-content').hide();
-				el.find('.widget-name').addClass('widget-name-closed');			
+				el.find('.widget-name').addClass('widget-name-closed');
 			});
 		},
-		
-		// Accordion		
+
+		// Accordion
 		accordion : function()
 		{
 			return this.each(function(){
 				var el = $(this);
-				
+
 				// Set it up
 				el.find('.element-content').hide();
 				el.find('.element-content:first').show();
-				
+
 				// The click
 				el.find('.element-trigger').click(function(){
 					var anchor = $(this);
-					if( ! anchor.hasClass('active') ) 
+					if( ! anchor.hasClass('active') )
 					{
 						el.find('.element-content').hide();
 						el.find('.element-trigger').removeClass('active');
@@ -375,12 +375,12 @@
 						anchor.next('.element-content').show();
 					}
 					return false;
-				});	
+				});
 			});
 		}
 
 	};
-	
+
 	// Setup themeblvd namespace
 	$.fn.themeblvd = function(method) {
 		if( themeblvd_shared[method] )
@@ -388,7 +388,7 @@
 		else if( typeof method === 'object' || ! method )
 			return themeblvd_shared.init.apply( this, arguments );
 		else
-			$.error( 'Method ' +  method + ' does not exist.' ); 
+			$.error( 'Method ' +  method + ' does not exist.' );
 	};
 
 })(jQuery);
@@ -399,7 +399,7 @@
 
 (function($) {
 	themeblvd_media_uploader = {
-		
+
 		/**
 		 * Apply click actions initially when loaded.
 		 */
@@ -408,7 +408,7 @@
 			options.find('.upload-button').click(function(event){
 				themeblvd_media_uploader.add_file( $(this).closest('.section-upload') );
 			});
-			
+
 			options.find('.remove-image, .remove-file').click(function(){
 				themeblvd_media_uploader.remove_file( $(this).closest('.section-upload') );
 			});
@@ -431,7 +431,7 @@
 			if( upload_type == 'video' )
 				media_type = 'video';
 
-			event.preventDefault();			
+			// event.preventDefault();
 
 			// Create the media frame.
 			file_frame = wp.media.frames.file_frame = wp.media({
@@ -449,12 +449,12 @@
 
 			// Image selected and inserted
 			file_frame.on( 'select', function() {
-				
+
 				// Grab the selected attachment.
 				var attachment = file_frame.state().get('selection').first(),
 					remove_text = current_option.find('.trigger').data('remove'),
 					helper_text;
-				
+
 				current_option.find('.image-url').val(attachment.attributes.url);
 				if( attachment.attributes.type == 'image' )
 					current_option.find('.screenshot').empty().hide().append('<img src="' + attachment.attributes.url + '"><a class="remove-image">Remove</a>').slideDown('fast');
@@ -479,14 +479,14 @@
 				{
 					current_option.find('.upload-button').unbind().addClass('remove-file').removeClass('upload-button').val(remove_text);
 					current_option.find('.of-background-properties').slideDown();
-					
+
 					current_option.find('.remove-image, .remove-file').click(function() {
 						themeblvd_media_uploader.remove_file( $(this).closest('.section-upload') );
 			        });
 				}
 			});
 
-			// Modal window closed w/no insertion of an image. So, we need to 
+			// Modal window closed w/no insertion of an image. So, we need to
 			// reset the upload button to avoid weird results.
 			file_frame.on( 'close', function() {
 				current_option.find('.upload-button').unbind('click');
@@ -506,7 +506,7 @@
 		{
 			var upload_text = current_option.find('.trigger').data('upload'),
 				upload_type = current_option.find('.trigger').data('type');
-			
+
 			if( upload_type == 'slider' )
 				current_option.closest('.widget').find('.slide-summary').removeClass('image video').hide().text('');
 
@@ -520,24 +520,24 @@
 			});
 		}
 	};
-})(jQuery);	
+})(jQuery);
 
 /**
  * Show alert popup. Used for warnings and confirmations,
  * mostly intended to be used with AJAX actions.
  */
- 
+
 (function($) {
 	tbc_alert = {
 		init : function(alert_text, alert_class, selector)
 		{
-		
+
 		  	// Available classes:
 			// success (green)
 			// reset (red)
 			// warning (yellow)
-			
-			// Initial HTML markup	
+
+			// Initial HTML markup
 			var alert_markup	= '	<div id="tb-alert"> \
 										<div class="tb-alert-inner">  \
 											<div class="tb-alert-pad">  \
@@ -547,39 +547,39 @@
 											</div><!-- .tb-alert-pad (end) --> \
 										</div><!-- .tb-alert-inner (end) --> \
 									 </div><!-- .tb-alert (end) -->';
-			
-			// Add initial markup to site				
+
+			// Add initial markup to site
 			if( ! selector )
 				selector = 'body';
-				
+
 			$(selector).append(alert_markup);
-			
+
 			var	$this 			= $('#tb-alert'),
 				window_height 	= $(window).height();
-			
+
 			// Insert dynamic elements into markup
 			$this.addClass('tb-'+alert_class);
 			$this.find('.tb-alert-message p').text(alert_text);
-			
+
 			// Position it, but only if it's being applied to the entire body.
 			if( selector == 'body' )
 			{
 				$this.animate({'top' : ( window_height - ( window_height-75 ) ) + $(window).scrollTop() + "px"}, 100);
 			}
-			
+
 			// Show it and fade it out 1.5 secs later
 			$this.fadeIn(500, function(){
 				setTimeout( function(){
 					$this.fadeOut(500, function(){
 						$this.remove();
 					});
-					
+
 		      	}, 1500);
 			});
 
 		}
 	};
-})(jQuery);	
+})(jQuery);
 
 /**
  * Confirmation
@@ -587,7 +587,7 @@
 
 (function($){
 	tbc_confirm = function(string, args, callback)
-	{	
+	{
 		var default_args =
 			{
 			'confirm'		:	false, 		// Ok and Cancel buttons
@@ -599,15 +599,15 @@
 			'textYes'		:	'Yes',		// Yes button default text
 			'textNo'		:	'No'		// No button default text
 			}
-	
-		if(args) 
+
+		if(args)
 		{
-			for(var index in default_args) 
-			{ 
+			for(var index in default_args)
+			{
 				if(typeof args[index] == "undefined") args[index] = default_args[index];
-			} 
+			}
 		}
-	
+
 		var aHeight = $(document).height();
 		var aWidth = $(document).width();
 		$('body').append('<div class="appriseOverlay" id="aOverlay"></div>');
@@ -616,25 +616,25 @@
 		$('.appriseOuter').append('<div class="appriseInner"></div>');
 		$('.appriseInner').append(string);
 		$('.appriseOuter').css("left", ( $(window).width() - $('.appriseOuter').width() ) / 2+$(window).scrollLeft() + "px");
-	
+
 		if(args)
 		{
 			if(args['animate'])
-			{ 
+			{
 				var aniSpeed = args['animate'];
 				if(isNaN(aniSpeed)) { aniSpeed = 400; }
 				$('.appriseOuter').css('top', '-200px').show().animate({top:"100px"}, aniSpeed);
 			}
 			else
-			{ 
+			{
 				$('.appriseOuter').css('top', '100px').fadeIn(200);
 			}
 		}
 		else
-		{ 
+		{
 			$('.appriseOuter').css('top', '100px').fadeIn(200);
 		}
-	
+
 		if(args)
 		{
 			if(args['input'])
@@ -650,14 +650,14 @@
 				$('.aTextbox').focus();
 			}
 		}
-	
+
 		$('.appriseInner').append('<div class="aButtons"></div>');
 		if(args)
 		{
 			if(args['confirm'] || args['input'])
-			{ 
+			{
 				$('.aButtons').append('<button value="ok">'+args['textOk']+'</button>');
-				$('.aButtons').append('<button value="cancel">'+args['textCancel']+'</button>'); 
+				$('.aButtons').append('<button value="cancel">'+args['textCancel']+'</button>');
 			}
 			else if(args['verify'])
 			{
@@ -673,29 +673,29 @@
 		{
 			$('.aButtons').append('<button value="ok">Ok</button>');
 		}
-	
-		$(document).keydown(function(e) 
+
+		$(document).keydown(function(e)
 		{
 			if($('.appriseOverlay').is(':visible'))
 			{
-				if(e.keyCode == 13) 
-				{ 
+				if(e.keyCode == 13)
+				{
 					$('.aButtons > button[value="ok"]').click();
 				}
-				if(e.keyCode == 27) 
+				if(e.keyCode == 27)
 				{
 					$('.aButtons > button[value="cancel"]').click();
 				}
 			}
 		});
-	
+
 		var aText = $('.aTextbox').val();
 		if(!aText) { aText = false; }
 		$('.aTextbox').keyup(function()
-		{ 
-			aText = $(this).val(); 
+		{
+			aText = $(this).val();
 		});
-	
+
 		$('.aButtons > button').click(function()
 		{
 			$('.appriseOverlay').remove();
@@ -704,25 +704,25 @@
 			{
 				var wButton = $(this).attr("value");
 				if(wButton=='ok')
-				{ 
+				{
 					if(args)
 					{
 						if(args['input'])
-						{ 
+						{
 							callback(aText);
 						}
 						else
-						{ 
+						{
 							callback(true);
 						}
 					}
 					else
-					{ 
+					{
 						callback(true);
 					}
 				}
 				else if(wButton=='cancel')
-				{ 
+				{
 					callback(false);
 				}
 			}
@@ -742,14 +742,14 @@ jQuery(document).ready(function($) {
 	        {
 	        	// Add in reset so our sanitizaiton callback reconizes.
 	        	$('#themeblvd_options_page').append('<input type="hidden" name="reset" value="true" />');
-	        	
+
 	        	// Submit form
 	        	$('#themeblvd_options_page').submit();
 	        }
 	    });
 	    return false;
 	});
-	
+
 	$('#themeblvd_options_page .clear-button').click(function(){
 		tbc_confirm('<h3>'+themeblvd.clear_title+'</h3>'+themeblvd.clear, {'confirm':true}, function(r)
 		{
@@ -759,13 +759,13 @@ jQuery(document).ready(function($) {
 	        	var form = $('#themeblvd_options_page'),
 	        		option_id = form.find('input[name="option_page"]').val();
 
-	        	// Clear form's action so we don't go to options.php 
+	        	// Clear form's action so we don't go to options.php
 	        	// and WP Settings API handling.
 	        	form.attr('action', '');
 
 	        	// Add in reset so our sanitizaiton callback reconizes.
 	        	form.append('<input type="hidden" name="themeblvd_clear_options" value="'+option_id+'" />');
-	        	
+
 	        	// Submit form
 	        	form.submit();
 
