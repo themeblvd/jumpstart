a2e931b6a7a042a99df92ef7ec6cc70bab519515
Jason Bobich <jasonbobich@gmail.com>
2013-09-20 20:02:22 -0800

    Whitespace cleanup in admin JS files.

--- framework/admin/assets/js/meta-box.js
+++ framework/admin/assets/js/meta-box.js
@@ -3,15 +3,15 @@
  */
 
 jQuery(document).ready(function($) {
-	
+
 	/*-----------------------------------------------------------------------------------*/
 	/* Hi-jacked Page Attributes meta box
 	/*-----------------------------------------------------------------------------------*/
-	
+
 	// Show the proper option on page load
 	var page_atts = $('#themeblvd_pageparentdiv'),
 		template = page_atts.find('select[name="page_template"]').val();
-	
+
 	if( template == 'template_builder.php' )
 	{
 		page_atts.find('select[name="_tb_sidebar_layout"]').hide().prev('p').hide();
@@ -20,7 +20,7 @@ jQuery(document).ready(function($) {
 	{
 		page_atts.find('p.tb_custom_layout').hide().prev('p').hide();
 	}
-	
+
 	// Show the proper option when user changes <select>
 	page_atts.find('select[name="page_template"]').change(function(){
 		var template = $(this).val();
@@ -35,25 +35,25 @@ jQuery(document).ready(function($) {
 			page_atts.find('select[name="_tb_sidebar_layout"]').show().prev('p').show();
 		}
 	});
-	
+
 	/*-----------------------------------------------------------------------------------*/
 	/* Options Framework imports for meta boxes
 	/*-----------------------------------------------------------------------------------*/
-	
+
 	// Image Options
 	$('.of-radio-img-img').click(function(){
 		$(this).parent().parent().find('.of-radio-img-img').removeClass('of-radio-img-selected');
-		$(this).addClass('of-radio-img-selected');		
+		$(this).addClass('of-radio-img-selected');
 	});
-		
+
 	$('.of-radio-img-label').hide();
 	$('.of-radio-img-img').show();
 	$('.of-radio-img-radio').hide();
-	
+
 	/*-----------------------------------------------------------------------------------*/
 	/* Edit Post Meta Box
 	/*-----------------------------------------------------------------------------------*/
-	
+
 	// Setup featured image link
 	var value, meta_box = $('.tb-meta-box');
 	meta_box.find('.tb-thumb-link').hide();
