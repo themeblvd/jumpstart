ca9a99d40424c2525675304ff3934a82917427c4
Jason Bobich <jasonbobich@gmail.com>
2013-12-06 16:28:55 -0600

    Search form updates for Bootstrap 3 (#100)

--- searchform.php
+++ searchform.php
@@ -1,9 +1,12 @@
-<?php $text = themeblvd_get_local( 'search' ); ?>
 <div class="themeblvd-search">
     <form method="get" action="<?php echo home_url( '/' ); ?>">
-		<div class="controls">
-			<input type="text" class="search-input" name="s" placeholder="<?php echo $text; ?>" />
-			<button class="btn btn-default" type="submit"><i class="icon-search"></i></button>
+		<div class="input-group">
+			<input type="text" class="form-control search-input" name="s" placeholder="<?php echo themeblvd_get_local( 'search' ); ?>" />
+			<span class="input-group-btn">
+				<button class="<?php echo themeblvd_get_button_class( 'default' ); ?>" type="submit">
+					<i class="icon-search"></i>
+				</button>
+			</span>
 		</div>
-    </form>
+	</form>
 </div>
