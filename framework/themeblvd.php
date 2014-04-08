7b19b09af895c73ff1e8f158d1fe107660ace0f8
Jason Bobich <jasonbobich@gmail.com>
2014-04-08 09:36:45 -0800

    Version bump

--- framework/themeblvd.php
+++ framework/themeblvd.php
@@ -10,7 +10,7 @@
  */
 
 // Constants
-define( 'TB_FRAMEWORK_VERSION', '2.4.3' );
+define( 'TB_FRAMEWORK_VERSION', '2.4.4' );
 define( 'TB_FRAMEWORK_DIRECTORY', get_template_directory().'/framework' );
 define( 'TB_FRAMEWORK_URI', get_template_directory_uri().'/framework' );
 
