6d78c5d94fba049ab944ed2cccf3f4427fedb8fe
Jason Bobich <jasonbobich@gmail.com>
2014-06-04 15:33:23 -0800

    Added "Milestones" element

--- framework/assets/js/themeblvd.js
+++ framework/assets/js/themeblvd.js
@@ -1,66 +1,4 @@
 // ---------------------------------------------------------
-// Responsive navigation menu.
-// ---------------------------------------------------------
-
-(function($) {
-	"use strict";
-    $.fn.toggleMenu = function(options) {
-
-        var settings = $.extend({
-			'viewport'		: 768,
-			'openClass'		: 'mobile-open',
-			'closedClass'	: 'mobile-closed'
-		}, options);
-
-        // Run it
-        this.each(function(){
-
-			var el = $(this),
-				target = $(el.attr('href')),
-				currentViewport,
-				timeout = false;
-
-			// Toggle on click
-			el.click(function(){
-				if (  target.hasClass(settings.openClass) )
-					target.slideUp().removeClass(settings.openClass).addClass(settings.closedClass);
-				else
-					target.slideDown().removeClass(settings.closedClass).addClass(settings.openClass);
-
-				return false;
-			});
-
-			// Window re-sizing  - For those people screwing with the
-			// browser window and are not actually on a mobile device.
-		    $( window ).resize( function() {
-				if ( false !== timeout )
-					clearTimeout( timeout );
-
-				timeout = setTimeout( function() {
-					currentViewport = $(window).width();
-					if (  currentViewport > settings.viewport ) {
-
-						// Add class "expanded" so we can keep track of
-						// whether this re-sizing is occuring on a mobile
-						// device or not. If we're on mobile, the "forced_open"
-						// class should never get added.
-						target.show().removeClass(settings.openClass+' '+settings.closedClass).addClass('expanded');
-
-					} else {
-
-						// Make sure this wasn't triggered by re-sizing on mobile
-						if (  target.hasClass('expanded') )
-							target.hide().removeClass('expanded');
-
-					}
-
-				}, 100 );
-			});
-        });
-    };
-})(jQuery);
-
-// ---------------------------------------------------------
 // Frontend jQuery on DOM load
 // ---------------------------------------------------------
 
@@ -415,4 +353,320 @@ jQuery(document).ready(function($) {
 
 	}
 
+	// ---------------------------------------------------------
+	// Milestones
+	// ---------------------------------------------------------
+
+	$('.desktop .tb-milestones .milestone').each(function() {
+		$(this).appear(function() {
+
+			var $el = $(this),
+				num = parseInt($el.find('.num').text());
+
+			$el.find('.num').countTo({
+				from: 0,
+				to: num,
+				speed: 1500,
+				refreshInterval: 30
+			});
+
+		},{accX: 0, accY: 0});
+	});
+
 });
+
+// ---------------------------------------------------------
+// Allow animation when items appear w/scroll
+// script by Michael Hixson and Alexander Brovikov
+// ---------------------------------------------------------
+
+(function($) {
+    $.fn.appear = function(fn, options) {
+
+        var settings = $.extend({
+
+            //arbitrary data to pass to fn
+            data: undefined,
+
+            //call fn only on the first appear?
+            one: true,
+
+            // X & Y accuracy
+            accX: 0,
+            accY: 0
+
+        }, options);
+
+        return this.each(function() {
+
+            var t = $(this);
+
+            //whether the element is currently visible
+            t.appeared = false;
+
+            if (!fn) {
+
+                //trigger the custom event
+                t.trigger('appear', settings.data);
+                return;
+            }
+
+            var w = $(window);
+
+            //fires the appear event when appropriate
+            var check = function() {
+
+                //is the element hidden?
+                if (!t.is(':visible')) {
+
+                    //it became hidden
+                    t.appeared = false;
+                    return;
+                }
+
+                //is the element inside the visible window?
+                var a = w.scrollLeft();
+                var b = w.scrollTop();
+                var o = t.offset();
+                var x = o.left;
+                var y = o.top;
+
+                var ax = settings.accX;
+                var ay = settings.accY;
+                var th = t.height();
+                var wh = w.height();
+                var tw = t.width();
+                var ww = w.width();
+
+                if (y + th + ay >= b &&
+                    y <= b + wh + ay &&
+                    x + tw + ax >= a &&
+                    x <= a + ww + ax) {
+
+                    //trigger the custom event
+                    if (!t.appeared) t.trigger('appear', settings.data);
+
+                } else {
+
+                    //it scrolled out of view
+                    t.appeared = false;
+                }
+            };
+
+            //create a modified fn with some additional logic
+            var modifiedFn = function() {
+
+                //mark the element as visible
+                t.appeared = true;
+
+                //is this supposed to happen only once?
+                if (settings.one) {
+
+                    //remove the check
+                    w.unbind('scroll', check);
+                    var i = $.inArray(check, $.fn.appear.checks);
+                    if (i >= 0) $.fn.appear.checks.splice(i, 1);
+                }
+
+                //trigger the original fn
+                fn.apply(this, arguments);
+            };
+
+            //bind the modified fn to the element
+            if (settings.one) t.one('appear', settings.data, modifiedFn);
+            else t.bind('appear', settings.data, modifiedFn);
+
+            //check whenever the window scrolls
+            w.scroll(check);
+
+            //check whenever the dom changes
+            $.fn.appear.checks.push(check);
+
+            //check now
+            (check)();
+        });
+    };
+
+    //keep a queue of appearance checks
+    $.extend($.fn.appear, {
+
+        checks: [],
+        timeout: null,
+
+        //process the queue
+        checkAll: function() {
+            var length = $.fn.appear.checks.length;
+            if (length > 0) while (length--) ($.fn.appear.checks[length])();
+        },
+
+        //check the queue asynchronously
+        run: function() {
+            if ($.fn.appear.timeout) clearTimeout($.fn.appear.timeout);
+            $.fn.appear.timeout = setTimeout($.fn.appear.checkAll, 20);
+        }
+    });
+
+    //run checks when these methods are called
+    $.each(['append', 'prepend', 'after', 'before', 'attr',
+        'removeAttr', 'addClass', 'removeClass', 'toggleClass',
+        'remove', 'css', 'show', 'hide'], function(i, n) {
+        var old = $.fn[n];
+        if (old) {
+            $.fn[n] = function() {
+                var r = old.apply(this, arguments);
+                $.fn.appear.run();
+                return r;
+            }
+        }
+    });
+
+})(jQuery);
+
+// ---------------------------------------------------------
+// Animation for number counting
+// script by Matt Huggins
+// ---------------------------------------------------------
+
+(function ($) {
+	$.fn.countTo = function (options) {
+		options = options || {};
+
+		return $(this).each(function () {
+			// set options for current element
+			var settings = $.extend({}, $.fn.countTo.defaults, {
+				from:            $(this).data('from'),
+				to:              $(this).data('to'),
+				speed:           $(this).data('speed'),
+				refreshInterval: $(this).data('refresh-interval'),
+				decimals:        $(this).data('decimals')
+			}, options);
+
+			// how many times to update the value, and how much to increment the value on each update
+			var loops = Math.ceil(settings.speed / settings.refreshInterval),
+				increment = (settings.to - settings.from) / loops;
+
+			// references & variables that will change with each update
+			var self = this,
+				$self = $(this),
+				loopCount = 0,
+				value = settings.from,
+				data = $self.data('countTo') || {};
+
+			$self.data('countTo', data);
+
+			// if an existing interval can be found, clear it first
+			if (data.interval) {
+				clearInterval(data.interval);
+			}
+			data.interval = setInterval(updateTimer, settings.refreshInterval);
+
+			// initialize the element with the starting value
+			render(value);
+
+			function updateTimer() {
+				value += increment;
+				loopCount++;
+
+				render(value);
+
+				if (typeof(settings.onUpdate) == 'function') {
+					settings.onUpdate.call(self, value);
+				}
+
+				if (loopCount >= loops) {
+					// remove the interval
+					$self.removeData('countTo');
+					clearInterval(data.interval);
+					value = settings.to;
+
+					if (typeof(settings.onComplete) == 'function') {
+						settings.onComplete.call(self, value);
+					}
+				}
+			}
+
+			function render(value) {
+				var formattedValue = settings.formatter.call(self, value, settings);
+				$self.text(formattedValue);
+			}
+		});
+	};
+
+	$.fn.countTo.defaults = {
+		from: 0,               // the number the element should start at
+		to: 0,                 // the number the element should end at
+		speed: 1000,           // how long it should take to count between the target numbers
+		refreshInterval: 100,  // how often the element should be updated
+		decimals: 0,           // the number of decimal places to show
+		formatter: formatter,  // handler for formatting the value before rendering
+		onUpdate: null,        // callback method for every time the element is updated
+		onComplete: null       // callback method for when the element finishes updating
+	};
+
+	function formatter(value, settings) {
+		return value.toFixed(settings.decimals);
+	}
+}(jQuery));
+
+// ---------------------------------------------------------
+// Responsive navigation menu.
+// script by Jason Bobich
+// ---------------------------------------------------------
+
+(function($) {
+	"use strict";
+    $.fn.toggleMenu = function(options) {
+
+        var settings = $.extend({
+			'viewport'		: 768,
+			'openClass'		: 'mobile-open',
+			'closedClass'	: 'mobile-closed'
+		}, options);
+
+        // Run it
+        this.each(function(){
+
+			var el = $(this),
+				target = $(el.attr('href')),
+				currentViewport,
+				timeout = false;
+
+			// Toggle on click
+			el.click(function(){
+				if (  target.hasClass(settings.openClass) )
+					target.slideUp().removeClass(settings.openClass).addClass(settings.closedClass);
+				else
+					target.slideDown().removeClass(settings.closedClass).addClass(settings.openClass);
+
+				return false;
+			});
+
+			// Window re-sizing  - For those people screwing with the
+			// browser window and are not actually on a mobile device.
+		    $( window ).resize( function() {
+				if ( false !== timeout )
+					clearTimeout( timeout );
+
+				timeout = setTimeout( function() {
+					currentViewport = $(window).width();
+					if (  currentViewport > settings.viewport ) {
+
+						// Add class "expanded" so we can keep track of
+						// whether this re-sizing is occuring on a mobile
+						// device or not. If we're on mobile, the "forced_open"
+						// class should never get added.
+						target.show().removeClass(settings.openClass+' '+settings.closedClass).addClass('expanded');
+
+					} else {
+
+						// Make sure this wasn't triggered by re-sizing on mobile
+						if (  target.hasClass('expanded') )
+							target.hide().removeClass('expanded');
+
+					}
+
+				}, 100 );
+			});
+        });
+    };
+})(jQuery);
