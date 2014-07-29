61b948eebb483bcba7d9b1403e6f84de579f85ba
Jason Bobich <jasonbobich@gmail.com>
2014-07-28 16:13:10 -0800

    Fixed redirect page template to work with https

--- template_redirect.php
+++ template_redirect.php
@@ -23,13 +23,15 @@
  * @package 	Theme Blvd WordPress Framework
  */
 ?>
-<?php if ( have_posts() ) : the_post(); ?>
-<?php $URL = get_the_excerpt(); if (!preg_match('/^http:\/\//', $URL)) $URL = 'http://' . $URL; ?>
-
+<?php
+if ( have_posts() ) : the_post();
+$url = get_the_excerpt();
+if ( ! preg_match('/^http:\/\//', $url) && ! preg_match('/^https:\/\//', $url) ) $url = 'http://' . $url;
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Refresh" content="0; url=<?php echo $URL; ?>">
+<meta http-equiv="Refresh" content="0; url=<?php echo $url; ?>">
 </head>
 
 <body>
