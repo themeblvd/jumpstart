ec0fa31dbe030b6b009e0c0f0baa6361b298e8d4
Jason Bobich <jasonbobich@gmail.com>
2014-08-06 17:10:34 -0800

    Since btn no longer has corners, add to searchform

--- searchform.php
+++ searchform.php
@@ -2,7 +2,7 @@
     <form method="get" action="<?php echo home_url('/'); ?>">
 		<div class="search-wrap">
 			<input type="text" class="search-input" name="s" placeholder="<?php echo themeblvd_get_local('search'); ?>" />
-			<button class="search-submit primary" type="submit">
+			<button class="search-submit btn-primary" type="submit">
 				<i class="fa fa-search"></i>
 			</button>
 		</div>
