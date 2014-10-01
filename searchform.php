4afb40688554f484c386a3d38b47be4312509f81
Jason Bobich <jasonbobich@gmail.com>
2014-10-01 11:19:47 -0700

    Better mobile search

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <div class="tb-search">
     <form method="get" action="<?php echo esc_url( trailingslashit(themeblvd_get_home_url()) ); ?>">
 		<div class="search-wrap">
-			<input type="text" class="search-input" name="s" placeholder="<?php echo themeblvd_get_local('search'); ?>" />
+			<input type="search" class="search-input" name="s" placeholder="<?php echo themeblvd_get_local('search'); ?>" />
 			<button class="search-submit btn-primary" type="submit">
 				<i class="fa fa-search"></i>
 			</button>
