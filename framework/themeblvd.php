9856a11aab3c4a2cc6613995ad6b3210aa7538d6
Jason Bobich <jasonbobich@gmail.com>
2014-11-01 12:08:22 -0800

    Remove presstrends tracking

--- framework/themeblvd.php
+++ framework/themeblvd.php
@@ -82,7 +82,6 @@ if ( is_admin() ) {
 	add_action( 'admin_init', 'themeblvd_disable_nag' );
 	add_action( 'admin_init', 'themeblvd_clear_options' );
 	add_action( 'themeblvd_options_footer_text', 'themeblvd_options_footer_text_default' );
-	add_action( 'admin_init', 'themeblvd_stats' );
 	add_action( 'admin_init', 'themeblvd_update_version' );
 	add_action( 'admin_menu', 'themeblvd_hijack_page_atts' );
 	add_action( 'save_post', 'themeblvd_save_page_atts' );
