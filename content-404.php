d7e20a86b6e16880d2c8b328f98cb6e894e9d1b5
Jason Bobich <jasonbobich@gmail.com>
2015-03-22 13:26:59 -0800

    404 output - more consistent to standard pages

--- content-404.php
+++ content-404.php
@@ -5,7 +5,7 @@
 ?>
 <article class="404 bg-content">
 	<div class="entry-content">
-		<h1><?php echo themeblvd_get_local( '404_title' ); ?></h1>
-		<?php echo themeblvd_get_local( '404' ); ?>
+		<h1 class="entry-title"><?php echo themeblvd_get_local('404_title'); ?></h1>
+		<p><?php echo themeblvd_get_local('404'); ?></p>
 	</div><!-- .entry-content -->
 </article>
