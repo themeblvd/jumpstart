51af0a5334e6cc58fa3de38558202af9f6b343f5
Jason Bobich <jasonbobich@gmail.com>
2015-04-20 13:20:40 -0800

    Updated to Bootstrap 3.3.4

--- framework/includes/frontend.php
+++ framework/includes/frontend.php
@@ -361,7 +361,7 @@ function themeblvd_include_scripts() {
 
 	if ( themeblvd_supports( 'assets', 'bootstrap' ) ) {
 		$scripts[] = 'bootstrap';
-		wp_enqueue_script( 'bootstrap', TB_FRAMEWORK_URI . '/assets/plugins/bootstrap/js/bootstrap.min.js', array('jquery'), '3.3.0' );
+		wp_enqueue_script( 'bootstrap', TB_FRAMEWORK_URI . '/assets/plugins/bootstrap/js/bootstrap.min.js', array('jquery'), '3.3.4' );
 	}
 
 	if ( themeblvd_supports( 'assets', 'magnific_popup' ) ) {
