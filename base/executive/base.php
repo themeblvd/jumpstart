0cade3778e6ced30dba4c0a8c5ad522044fe3bea
Jason Bobich <jasonbobich@gmail.com>
2016-04-05 17:19:12 -0800

    Added font-size option for epic thumbnails

--- base/executive/base.php
+++ base/executive/base.php
@@ -1689,6 +1689,12 @@ function jumpstart_ex_css() {
 
 		$print .= "}\n";
 
+		$print .= "@media (min-width: 992px) {\n";
+		$print .= "\t.epic-thumb .epic-thumb-header .entry-title {\n";
+		$print .= sprintf("\tfont-size: %s;\n", themeblvd_get_font_size($font) );
+		$print .= "\t}\n";
+		$print .= "}\n";
+
 	}
 
 	// Links
