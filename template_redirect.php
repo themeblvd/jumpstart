f6ed235374d1c6685239419679b7764598cc2ed8
Jason Bobich <jasonbobich@gmail.com>
2016-06-23 11:24:24 -0800

    Fixed: Redirect page template not redirecting properly.

--- template_redirect.php
+++ template_redirect.php
@@ -25,7 +25,7 @@
 ?>
 <?php
 if ( have_posts() ) : the_post();
-$url = get_the_excerpt();
+$url = get_the_content();
 if ( ! preg_match('/^http:\/\//', $url) && ! preg_match('/^https:\/\//', $url) ) $url = 'http://' . $url;
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
