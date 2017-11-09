b61efab27a6618fff0a1a378cf259d1f2e3f3d5b
Jason Bobich <jasonbobich@gmail.com>
2017-11-09 12:14:50 -0700

    WP coding standards pass for Theme_Blvd_Compat_Subtitles #223

--- dist/jumpstart/framework/compat/class-theme-blvd-compat-subtitles.php
+++ dist/jumpstart/framework/compat/class-theme-blvd-compat-subtitles.php
@@ -6,92 +6,114 @@
  * @copyright  2009-2017 Theme Blvd
  * @package    Jump_Start
  * @subpackage Theme_Blvd
- * @since      Theme_Blvd 2.5.0
+ * @since      Theme_Blvd 2.6.0
  */
 
 /**
  * Add extended Subtitles plugin compatibility.
  *
- * @author      Jason Bobich
- * @copyright   2009-2017 Theme Blvd
- * @link        http://themeblvd.com
- * @package     Jump_Start
+ * This class follows the singleton pattern,
+ * meaning it can only be instantiated in
+ * one instance.
+ *
+ * @since Theme_Blvd 2.6.0
  */
 class Theme_Blvd_Compat_Subtitles {
 
 	/**
 	 * A single instance of this class.
 	 *
-	 * @since 2.5.0
+	 * @since Theme_Blvd 2.6.0
 	 */
 	private static $instance = null;
 
 	/**
-     * Creates or returns an instance of this class.
-     *
-     * @since 2.6.0
-     *
-     * @return Theme_Blvd_Compat_bbPress A single instance of this class.
-     */
+	 * Creates or returns an instance of this class.
+	 *
+	 * @since Theme_Blvd 2.6.0
+	 *
+	 * @return Theme_Blvd_Compat_Subtitles A single instance of this class.
+	 */
 	public static function get_instance() {
 
-		if ( self::$instance == null ) {
-            self::$instance = new self;
-        }
+		if ( null === self::$instance ) {
 
-        return self::$instance;
+			self::$instance = new self;
+
+		}
+
+		return self::$instance;
+
 	}
 
 	/**
 	 * Constructor. Hook everything in.
 	 *
-	 * @since 2.6.0
+	 * @since Theme_Blvd 2.6.0
 	 */
 	public function __construct() {
 
-        $subtitles = Subtitles::getinstance();
+		$subtitles = Subtitles::getinstance();
 
-        // Add support with Portfolios plugin
+		// Add support with Portfolios plugin.
 		add_post_type_support( 'portfolio_item', 'subtitles' );
 
-        // Remove default styles
-		remove_action( 'wp_head', array($subtitles, 'subtitle_styling') );
+		// Remove default styles.
+		remove_action( 'wp_head', array( $subtitles, 'subtitle_styling' ) );
 
-        // Make sure subtitles only output on single
-        // posts and pages.
-        add_filter( 'subtitle_view_supported', array($this, 'view') );
+		/*
+		 * Make sure subtitles only output on single
+		 * posts and pages.
+		 */
+		add_filter( 'subtitle_view_supported', array( $this, 'view' ) );
 
-        // Make sure no subtitle shows in instances of the title
-        // lower in a post, like with the title of the comments,
-        // for example.
-        add_action( 'themeblvd_single_footer', array($this, 'single_footer') );
+		/*
+		 * Make sure no subtitle shows in instances of the title
+		 * lower in a post, like with the title of the comments,
+		 * for example.
+		 */
+		add_action( 'themeblvd_single_footer', array( $this, 'single_footer' ) );
 
 	}
 
-    /**
-	 * Make sure subtitles only output on single
-     * posts and pages.
+	/**
+	 * Make sure subtitles only outputs on single
+	 * posts and pages.
 	 *
-	 * @since 2.6.0
-	 */
-    public function view( $support ) {
-
-        if ( ! is_singular() || themeblvd_get_att('doing_second_loop') ) {
-    		$support = false;
-    	}
-
-        return $support;
-    }
-
-    /**
-	 * Make sure no subtitle shows in instances of the title
-     * lower in a post, like with the title of the comments,
-     * for example.
+	 * This method is filtered onto
+	 * `subtitle_view_supported` - 10
 	 *
-	 * @since 2.6.0
+	 * @since Theme_Blvd 2.6.0
+	 *
+	 * @param  bool $support Whether to display subtitle.
+	 * @return bool $support Whether to display subtitle.
 	 */
-    public function single_footer() {
-        add_filter('subtitle_view_supported', '__return_false');
-    }
+	public function view( $support ) {
+
+		if ( ! is_singular() || themeblvd_get_att( 'doing_second_loop' ) ) {
+
+			$support = false;
+
+		}
+
+		return $support;
+
+	}
+
+	/**
+	 * Make sure no subtitle shows in instances of the
+	 * title lower in a post, like with the title of
+	 * the comments, for example.
+	 *
+	 * This method is hooked to:
+	 * `themeblvd_single_footer` - 10
+	 *
+	 * @since Theme_Blvd 2.6.0
+	 */
+	public function single_footer() {
+
+		add_filter( 'subtitle_view_supported', '__return_false' );
+
+	}
 
 }
