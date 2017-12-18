77b47199ef38f1e402396d2f7816c3682bbc5845
Jason Bobich <jasonbobich@gmail.com>
2017-12-18 13:04:03 -0700

    Convert search icon to FA 5. #339

--- lib/themeblvd/searchform.php
+++ lib/themeblvd/searchform.php
@@ -15,10 +15,12 @@
 
 		<div class="search-wrap">
 
+			<i class="search-icon <?php echo esc_attr( themeblvd_get_icon_class( 'search' ) ); ?>"></i>
+
 			<input type="search" class="search-input" name="s" placeholder="<?php echo themeblvd_get_local( 'search' ); ?>" />
 
 			<button class="search-submit btn-primary" type="submit">
-				<i class="<?php echo esc_attr( themeblvd_get_icon_class( 'search' ) ); ?>"></i>
+				<?php echo esc_html( themeblvd_get_local( 'search_2' ) ); ?>
 			</button>
 
 		</div>
