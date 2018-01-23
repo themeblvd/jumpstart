16771debbf834c60faecd0956e9db18d19d9d30d
Jason Bobich <jasonbobich@gmail.com>
2018-01-22 21:42:34 -0700

    Added `themeblvd_slider()` back as deprecated function.

--- dist/jumpstart/framework/general/deprecated.php
+++ dist/jumpstart/framework/general/deprecated.php
@@ -35,3 +35,23 @@ if ( ! function_exists( 'themeblvd_responsive_menu_toggle' ) ) {
 
 	}
 }
+
+/**
+ * Display custom slider from Theme Blvd
+ * Sliders plugin.
+ *
+ * @since Theme_Blvd 2.0.0
+ * @deprecated Theme_Blvd 2.5.0
+ *
+ * @param string $slider Slug of custom-built slider to use.
+ */
+function themeblvd_slider( $slider ) {
+
+	themeblvd_deprecated_function(
+		__FUNCTION__,
+		'2.5.0',
+		null,
+		'The Theme Blvd Sliders plugin was deprecated as of Framework 2.5 and support for it was completely removed in Framework 2.7. Use one of the layout builder\'s slider elements or the [galler_slider] shortcode instead.'
+	);
+
+}
